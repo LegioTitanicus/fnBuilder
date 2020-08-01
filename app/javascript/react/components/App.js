a802ac2d1c96e144c682db0a1cb2b80ea162400c
LegioTitanicus <alexbernstein7170@gmail.com>
2020-08-01 17:29:28 -0400

    Installed MUI and tested button component displays with roboto font (CDN in <head>) set index.html.erb to the "app" div for react

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import Button from "@material-ui/core/Button";
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
-}
+  return (
+    <>
+      <h1>Make It So React</h1>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </>
+  );
+};
 
-export default App
+export default App;
