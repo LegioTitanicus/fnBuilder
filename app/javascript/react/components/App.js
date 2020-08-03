df1b889d361f922ed4b6486eb53c8e0419f7c52a
LegioTitanicus <alexbernstein7170@gmail.com>
2020-08-03 14:46:15 -0400

    Added mui and text fields to play with authentication

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Switch, BrowserRouter, Link } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+import Topnav from "./Topnav";
+import SignIn from "./SignIn";
+import { Paper } from "@material-ui/core";
+
 export const App = (props) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  // const [loginState, setLoginState] = useState({
+  //   username: "",
+  //   password: "",
+  // });
+  // const handleChangeUsername = (event) => {
+  //   setLoginState({
+  //     ...loginState,
+  //     [loginState.username]: event.currentTarget.value,
+  //   });
+  // };
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#263238",
+      },
+      secondary: {
+        main: "#18ffff",
+      },
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Topnav darkMode={darkMode} handleDarkMode={handleDarkMode} />
+          <Paper variant="outlined" style={{ height: "80vh" }}>
+            <Route exact path="/users/sign_in" component={SignIn} />
+            <Link to={`/users/sign_in`}>sign in</Link>
+          </Paper>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 };
---
+++ app/javascript/react/components/Topnav.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Brightness2Icon from "@material-ui/icons/Brightness2";
+import BrightnessMediumIcon from "@material-ui/icons/BrightnessMedium";
+import Switch from "@material-ui/core/Switch";
+import Button from "@material-ui/core/Button";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
+import Typography from "@material-ui/core/Typography";
+
+import { Grid } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import MuiDrawer from "./SwipeableDrawer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    // marginRight: theme.spacing(2),
+    marginLeft: "auto",
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Topnav = (props) => {
+  const { handleDarkMode, darkMode } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const classes = useStyles();
+
+  //   const isBig = useMediaQuery(theme.breakpoints.up("md"));  // will require I make a separate theme file so it can recognize breakpoints instead of the way I have it now in app.js
+
+  function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+  const handleDrawerToggle = () => {
+    console.log("toggle menu");
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <div className={classes.root}>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <MuiDrawer className={classes.menuButton} />
+            <Typography variant="h6" className={classes.title}>
+              {" "}
+              FnBuilder
+            </Typography>
+            {darkMode ? <Brightness2Icon /> : <BrightnessMediumIcon />}
+            <Switch
+              checked={darkMode}
+              onChange={handleDarkMode}
+              color="secondary"
+              name="checkedB"
+              inputProps={{ "aria-label": "primary checkbox" }}
+            />
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+    </div>
+  );
+};
+
+export default Topnav;
